fe3f18b1a6bb5c514b38577e005f4b3b3253f5e6
Pizz0x <filippopizzo03@gmail.com>
2025-04-24 12:46:17 +0200

    prova cambio account

--- RF.py
+++ RF.py
@@ -101,3 +101,4 @@ ConfusionMatrixDisplay.from_estimator(
 
 plt.show()
 # with the random forest we obtain a better model, but still with an accuracy that is very similiar to the majority class accuracy that is our baseline
+a = 1
